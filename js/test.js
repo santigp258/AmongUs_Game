8a89ae5a86c71c2aa23e80aaf1a603d201a45e12
santigp258 <santigp258@gmail.com>
2021-01-14 22:54:07 -0500

    validation

--- js/test.js
+++ js/test.js
@@ -8,19 +8,32 @@ Hacer un programa, funcionalidad de among us:
 -Si gana o pierde, lanzar una alerta
 */
 
-//function validate
-
+//functions
 function validation(users_consult, i) {
-  while (users_consult == "" || users_consult == null) {
+  while (!expresions.users.test(users_consult)) {
     users_array[i].name = prompt(
       "Añada el jugador número " + (i + 1) + " de forma correcta"
     );
 
-    if (users_array[i].name != "" && users_array[i].name != null) {
+    if (expresions.users.test(users_array[i].name)) {
       break;
     }
   }
 }
+function showUsers() {
+  document.write("<h4>Players:</h4><ul>");
+
+  users_array.forEach((element, index) => {
+    document.write(`<li>${index + 1} - ${element.name} </li>`);
+  });
+
+  document.write("</ul>");
+}
+
+const expresions = {
+  users: /^[A-Za-z0-9_]{1,20}$/,
+  find_impostor: /^[1-5]{1}$/,
+};
 
 
 //array players
@@ -36,34 +49,30 @@ for (let i = 0; i < 5; i++) {
   users_array.push(users);
   validation(users_consult, i);
 }
- 
+
 //show players
-document.write('<h4>Players:</h4><ul>')
-
-users_array.forEach((element, index) => {
-    document.write(`<li>${index + 1} - ${element.name} </li>`)
-})
-
-document.write('</ul>');
+showUsers();
 
 //define impostor
-let num = Math.floor((Math.random() * 5) + 1);
+let num = Math.floor(Math.random() * 4 + 1); //generate random number from 1 to 5
 
-//const impostor = users_array[num].id;
+const impostor = users_array[num].id;
 
-//find impostor 
-
-
+console.log(impostor);
+//find impostor
 setTimeout(() => {
-    let find_impostor = parseInt(prompt("Who is the impostor?"));
-    while (isNaN(find_impostor)|| typeof find_impostor == 'string' || find_impostor <= 0 || find_impostor > users_array.length) {
-            find_impostor = parseInt(prompt("Say who is the impostor correctly"));
-
-        if (!isNaN(find_impostor) && typeof find_impostor !== 'string'  || find_impostor > 0 && find_impostor < users_array.length) {
-            break;
-        }
+  let find_impostor = parseInt(prompt("Who is the impostor?"));
+  while (!expresions.find_impostor.test(find_impostor)) {
+    find_impostor = parseInt(
+      prompt("Say who the impostor is. Choose numbers from 1 to 5")
+    );
+    if (expresions.find_impostor.test(find_impostor)) {
+      break;
     }
+  }
 
-    console.log(typeof find_impostor);
-    console.log(find_impostor);
+  
+  /*   console.log(typeof find_impostor);
+  console.log(find_impostor);
+  console.log(users_array.length); */
 }, 4000);
